3d8d99a600ce5d8fef98d692255188a9eca7f35f
Jaume Boneu Vidal <73638206+jaumeboneu@users.noreply.github.com>
2024-11-12 16:43:34 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    const overlay = document.getElementById('overlay');
+    menu.classList.toggle('show');
+    overlay.classList.toggle('show');
+  }
+  
