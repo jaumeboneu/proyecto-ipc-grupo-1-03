b00779c32e6f0ffca05f17df5097248152bd0299
Jaume Boneu Vidal <73638206+jaumeboneu@users.noreply.github.com>
2024-11-19 16:24:46 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,3 +5,24 @@ function toggleMenu() {
     overlay.classList.toggle('show');
   }
   
+// Función para actualizar la URL cuando se hace clic en un acordeón
+document.querySelectorAll('.accordion-button').forEach(function (button) {
+  button.addEventListener('click', function () {
+    // Obtener el ID del acordeón (por ejemplo, 'collapseOne', 'collapseTwo', etc.)
+    var targetId = this.getAttribute('data-bs-target').substring(1); // Quitar el símbolo '#'
+
+    // Actualizar la URL sin recargar la página
+    history.pushState(null, null, '#'+targetId);
+  });
+});
+
+// Al cargar la página, verificar si hay un fragmento en la URL y abrir el acordeón correspondiente
+document.addEventListener('DOMContentLoaded', function () {
+  var hash = window.location.hash;
+  if (hash) {
+    // Si el fragmento es válido, abrir el acordeón correspondiente
+    var acordeon = new bootstrap.Collapse(document.querySelector(hash), {
+      toggle: true // Abrir el acordeón
+    });
+  }
+});
